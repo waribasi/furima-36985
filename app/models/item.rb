c83e273aee929b63c06ed12db15eacc35a27fb3b
waribasi <kakuremafu@icloud.com>
2021-10-07 16:30:25 +0900

    image関係のgemをインストした

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 end
+
+#以下は後にitemsコントローラに記載する何か
+# def message_params
+#   params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+# end
