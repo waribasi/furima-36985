e2f2809491b8ae669a20cdfc73ee8aec6ae43937
waribasi <kakuremafu@icloud.com>
2021-10-07 16:01:11 +0900

    テーブル作成

---
+++ db/migrate/20211007063455_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :item,           null: false
+      t.text :exp,              null: false
+      t.integer :cate_id,       null: false
+      t.integer :status_id,     null: false
+      t.integer :del_fee_id,    null: false
+      t.integer :area_id,       null: false
+      t.integer :del_day_id,    null: false
+      t.integer :price,         null: false
+      t.references :user,       null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
