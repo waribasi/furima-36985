6b02c793802eba7458c2aee374ede40db0182ecd
waribasi <kakuremafu@icloud.com>
2021-10-08 12:33:41 +0900

    単体テストコード（下書き）

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+  image            {}
+  item             {'メロンパン'}
+  exp              {'メロンパンはもう飽きたー！'}
+  cate_id          {1}
+  status_id        {1}
+  del_fee_id       {1}
+  area_id          {1}
+  del_day_id       {1}
+  price            {2000}
+  association :user
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
+
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品登録' do
+
+    context '商品が登録できる時' do
+      it '必要事項が全て記入されたら登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品が登録できない時' do
+      it '画像が選択してない時' do
+      end
+    end
+    
+  end
 end
+
+
+# 以下はメモ欄ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+# pending "add some examples to (or delete) #{__FILE__}"
+# テストコード
+# bundle exec rspec spec/models/item_spec.rb
